ada8f08ed1e1870899a7c160ffb31dbb27fc3b51
Antonio Eloy <101589161+antonioaraujozup@users.noreply.github.com>
2022-03-29 09:55:36 -0300

    Criando entidade Livro.

---
+++ src/main/java/br/com/zup/edu/livraria/api/repository/model/Livro.java
@@ -0,0 +1,42 @@
+package br.com.zup.edu.livraria.api.repository.model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 200)
+    private String titulo;
+
+    @Column(nullable = false, length = 4000)
+    private String descricao;
+
+    @Column(nullable = false)
+    private LocalDate dataPublicacao;
+
+    private String isbn;
+
+    public Livro(String titulo, String descricao, LocalDate dataPublicacao, String isbn) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.dataPublicacao = dataPublicacao;
+        this.isbn = isbn;
+    }
+
+    @Deprecated
+    /**
+     * @deprecated Construtor para uso exclusivo do Hibernate.
+     */
+    public Livro() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+}
